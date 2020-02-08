623e85404d193b0b616483073ea6144e09c6fc41
Soveriegn5 <chrisgari@hotmail.com>
2020-02-08 03:08:58 -0800

    Finished flix2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Christopther Garcia on 2/8/20.
+//  Copyright © 2020 Christopther Garcia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
